b28f84efed33052606b6ad587bb26f89e404cb85
rajesh-sv <125382031+rajesh-sv@users.noreply.github.com>
2024-06-01 22:12:59 +0530

    Initial setup

---
+++ app.js
@@ -0,0 +1,76 @@
+require("dotenv").config()
+const createError = require("http-errors")
+const express = require("express")
+const path = require("path")
+const cookieParser = require("cookie-parser")
+const logger = require("morgan")
+const mongoose = require("mongoose")
+const compression = require("compression")
+const helmet = require("helmet")
+
+const indexRouter = require("./routes/index")
+const usersRouter = require("./routes/users")
+const catalogRouter = require("./routes/catalog")
+
+const app = express()
+
+// Set up mongoose connection
+mongoose.set("strictQuery", false)
+const mongoDB = process.env.MONGODB_URI
+function main() {
+  return mongoose.connect(mongoDB)
+}
+main().catch(console.log)
+
+// view engine setup
+app.set("views", path.join(__dirname, "views"))
+app.set("view engine", "pug")
+
+// helmet for web security
+app.use(
+  helmet.contentSecurityPolicy({
+    directives: {
+      "script-src": ["'self'", "code.jquery.com", "cdn.jsdelivr.net"],
+    },
+  })
+)
+
+// Set up rate limiter: maximum of twenty requests per minute
+const RateLimit = require("express-rate-limit")
+const limiter = RateLimit({
+  windowMs: 1 * 60 * 1000, // 1 minute
+  max: 20,
+})
+// Apply rate limiter to all requests
+app.use(limiter)
+
+app.use(logger("dev"))
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+app.use(cookieParser())
+
+app.use(compression())
+
+app.use(express.static(path.join(__dirname, "public")))
+
+app.use("/", indexRouter)
+app.use("/users", usersRouter)
+app.use("/catalog", catalogRouter)
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+  next(createError(404))
+})
+
+// error handler
+app.use(function (err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message
+  res.locals.error = req.app.get("env") === "development" ? err : {}
+
+  // render the error page
+  res.status(err.status || 500)
+  res.render("error")
+})
+
+module.exports = app
