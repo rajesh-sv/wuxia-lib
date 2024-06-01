3b370fb3e3bb3cf351140a092651eed4317f4088
rajesh-sv <125382031+rajesh-sv@users.noreply.github.com>
2024-06-01 23:30:36 +0530

    Fix typo in title of catalog page

--- controllers/bookController.js
+++ controllers/bookController.js
@@ -23,7 +23,7 @@ exports.index = asyncHandler(async (req, res, next) => {
   ])
 
   res.render("index", {
-    title: "Local Library Home",
+    title: "Wuxia Lib Home",
     book_count: numBooks,
     book_instance_count: numBookInstances,
     book_instance_available_count: numAvailableBookInstances,
